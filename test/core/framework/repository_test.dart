57eff31aed288d8a3274fe65cdf0b5c2c60eb167
cbalmeida <cbalmeida@gmail.com>
2023-02-08 07:44:02 -0500

    Initial commit.

--- test/core/framework/repository_test.dart
+++ test/core/framework/repository_test.dart
@@ -48,20 +48,6 @@ void main() {
     expect(entity2 != null, true);
     repository.dispose();
   });
-
-  test('MemoryRepository.put: deve obter o objeto passado no parametro do cache de forma assincrona', () async {
-    MemoryRepository repository = MemoryRepository();
-    var entity = FakeEntity(id: '1');
-    repository.put(entity);
-    expect(repository.cache.length, 0);
-    await Future.delayed(const Duration(milliseconds: 200), () {
-      expect(repository.cache.length, 1);
-    });
-
-    var entity2 = await repository.get(entity.id);
-    expect(entity2 != null, true);
-    repository.dispose();
-  });
 }
 
 class FakeEntity extends Entity {
